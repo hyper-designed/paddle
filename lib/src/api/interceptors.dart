f84228ef890312cd05f0eea48b6d6bbaa46956a2
Saad Ardati <saadodi44@gmail.com>
2025-05-24 21:23:56 +0300

    Add Content-Type header to Paddle request interceptor

--- lib/src/api/interceptors.dart
+++ lib/src/api/interceptors.dart
@@ -5,13 +5,16 @@ import 'package:chopper/chopper.dart';
 class PaddleEssentialHeadersInterceptor implements Interceptor {
   @override
   FutureOr<Response<BodyType>> intercept<BodyType>(Chain<BodyType> chain) {
-    final request = chain.request.copyWith(headers: {'Paddle-Version': '1'});
+    final request = chain.request.copyWith(
+      headers: {'Paddle-Version': '1', 'Content-Type': 'application/json'},
+    );
     return chain.proceed(request);
   }
 }
 
 class PaddleAuthHeadersInterceptor implements Interceptor {
   final String apiKey;
+
   PaddleAuthHeadersInterceptor({required this.apiKey});
 
   @override
